9cb52a64e7e33031214a81cea46aef1140916fdf
kumu <kumu@2dfire.com>
2024-10-26 20:03:30 +0800

    替换完自动格式化

--- src/command/wrap-with-consumer-command.ts
+++ src/command/wrap-with-consumer-command.ts
@@ -2,19 +2,25 @@
 import * as vscode from 'vscode';
 
 export async function wrapWithConsumerCommand(document: vscode.TextDocument, range: vscode.Range) {
-  const editor = vscode.window.activeTextEditor;
-  if (!editor) {
-    return;
-  }
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return;
+    }
 
-  // 选定范围内的代码
-  const selectedText = document.getText(range);
+    console.log(`wrapWithConsumerCommand start--->${range.start.line}:${range.start.character}`);
+    console.log(`wrapWithConsumerCommand end--->${range.end.line}:${range.end.character}`);
 
-  // 包裹选中代码
-  const wrappedText = `Consumer(\n  builder: (context, watch, child) {\n    return ${selectedText};\n  },\n)`;
+    // 选定范围内的代码
+    const selectedText = document.getText(range);
 
-  // 编辑文档，替换选定文本为包裹后的文本
-  await editor.edit(editBuilder => {
-    editBuilder.replace(range, wrappedText);
-  });
+    // 包裹选中代码
+    const wrappedText = `Consumer(\n  builder: (context, watch, child) {\n    return ${selectedText};\n  },\n)`;
+
+    // 编辑文档，替换选定文本为包裹后的文本
+    await editor.edit(editBuilder => {
+        editBuilder.replace(range, wrappedText);
+    });
+
+    // 使用 formatDocument 格式化整个文件
+    await vscode.commands.executeCommand('editor.action.formatDocument');
 }
